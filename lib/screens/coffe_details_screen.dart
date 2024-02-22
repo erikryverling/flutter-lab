ac0b91c9a7ccbcbbbe40a961c45f56565176206d
Erik R. Yverling <erik.r@yverling.se>
2024-02-22 14:52:57 +0100

    Remove hard-coded size in Coffee details screen

--- lib/screens/coffe_details_screen.dart
+++ lib/screens/coffe_details_screen.dart
@@ -4,9 +4,6 @@ import '../io/coffee.dart';
 
 import '../styles/spaces.dart' as spaces;
 
-double boxHeight = 160;
-double boxWidth = 200;
-
 class CoffeeDetailsScreen extends StatelessWidget {
   final Coffee coffee;
 
@@ -23,57 +20,52 @@ class CoffeeDetailsScreen extends StatelessWidget {
                       color: Theme.of(context).colorScheme.surfaceVariant,
                       borderRadius: BorderRadius.all(Radius.circular(10))),
                   padding: EdgeInsets.all(spaces.normal),
-                  // TODO Try to avoid this by having a layout that wraps around the text instead
-                  // and set margins on the text
-                  height: 140,
-                  width: 220,
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                  child: Wrap(
+                      direction: Axis.vertical,
                       children: [
-                        RichText(
-                          text: TextSpan(
-                            text: "Origin: ",
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyLarge!
-                                .copyWith(fontWeight: FontWeight.bold),
-                            children: <TextSpan>[
-                              TextSpan(
-                                  text: coffee.origin,
-                                  style: Theme.of(context).textTheme.bodyLarge),
-                            ],
-                          ),
-                        ),
-                        RichText(
-                          text: TextSpan(
-                            text: "Roaster: ",
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyLarge!
-                                .copyWith(fontWeight: FontWeight.bold),
-                            children: <TextSpan>[
-                              TextSpan(
-                                  text: coffee.roaster,
-                                  style: Theme.of(context).textTheme.bodyLarge),
-                            ],
-                          ),
-                        ),
-                        RichText(
-                          text: TextSpan(
-                            text: "Region: ",
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyLarge!
-                                .copyWith(fontWeight: FontWeight.bold),
-                            children: <TextSpan>[
-                              TextSpan(
-                                  text: coffee.region,
-                                  style: Theme.of(context).textTheme.bodyLarge),
-                            ],
-                          ),
-                        ),
-                      ]))));
+                    RichText(
+                      text: TextSpan(
+                        text: "Origin: ",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge!
+                            .copyWith(fontWeight: FontWeight.bold),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: coffee.origin,
+                              style: Theme.of(context).textTheme.bodyLarge),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: "Roaster: ",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge!
+                            .copyWith(fontWeight: FontWeight.bold),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: coffee.roaster,
+                              style: Theme.of(context).textTheme.bodyLarge),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: "Region: ",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge!
+                            .copyWith(fontWeight: FontWeight.bold),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: coffee.region,
+                              style: Theme.of(context).textTheme.bodyLarge),
+                        ],
+                      ),
+                    ),
+                  ]))));
     });
   }
 }
